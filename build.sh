59a9f80bfacb172f2857629ccf871d808f1b36a2
Sven Dowideit <SvenDowideit@home.org.au>
2015-02-17 13:39:09 +1000

    now, with more working-ness
    Signed-off-by: Sven Dowideit <SvenDowideit@home.org.au>

--- build.sh
+++ build.sh
@@ -7,6 +7,37 @@ usage() {
 	exit 1
 }
 
+if [[ -e MAINTAINERS ]]; then
+	cp  MAINTAINERS /docs/sources/humans.txt
+fi
+
+VERSION=$(cat VERSION) \
+    && MAJOR_MINOR="${VERSION%.*}" \
+    && for i in $(seq $MAJOR_MINOR -0.1 1.0); do \
+            echo "<li><a class='version' href='/v$i'>Version v$i</a></li>"; \
+    done > sources/versions.html_fragment \
+    && GIT_BRANCH=$(cat GIT_BRANCH) \
+    && GITCOMMIT=$(cat GITCOMMIT) \
+    && AWS_S3_BUCKET=$(cat AWS_S3_BUCKET) \
+    && BUILD_DATE=$(date) \
+    && sed -i "s/\$VERSION/$VERSION/g" theme/mkdocs/base.html \
+    && sed -i "s/\$MAJOR_MINOR/v$MAJOR_MINOR/g" theme/mkdocs/base.html \
+    && sed -i "s/\$GITCOMMIT/$GITCOMMIT/g" theme/mkdocs/base.html \
+    && sed -i "s/\$GIT_BRANCH/$GIT_BRANCH/g" theme/mkdocs/base.html \
+    && sed -i "s/\$BUILD_DATE/$BUILD_DATE/g" theme/mkdocs/base.html \
+    && sed -i "s/\$AWS_S3_BUCKET/$AWS_S3_BUCKET/g" theme/mkdocs/base.html
+
+exit 0
+# fails when there are none!
+cd sources
+files=$(rgrep --files-with-matches '{{ include ".*" }}')
+if [[ "$files" != "" ]]; then
+	rgrep --files-with-matches '{{ include ".*" }}' | xargs sed -i~ 's/{{ include "\(.*\)" }}/cat include\/\1/ge'
+fi
+cd ..
+
+#Stop here for now
+exit 0
 
 extrafiles=($(find . -name "mkdocs-*.yml"))
 extralines=()
@@ -55,3 +86,8 @@ do
 	echo "$line" >> mkdocs.yml
 
 done < <(cat "mkdocs.yml.bak")
+
+
+# remove `^---*` lines from md's
+find . -name "*.md" | xargs sed -i~ -n '/^---*/!p'
+
