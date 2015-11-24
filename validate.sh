c1db0e95cb7b0a1484d6806f6b117ff42d86d7fd
Sven Dowideit <SvenDowideit@home.org.au>
2015-11-24 18:37:17 +1000

    Send fake junit.xml for markdownlint and hugo build
    Signed-off-by: Sven Dowideit <SvenDowideit@home.org.au>

--- validate.sh
+++ validate.sh
@@ -1,8 +1,17 @@
 #!/bin/bash  
-set -e
-
 echo "running markdownlint"
 /usr/local/bin/markdownlint /docs/content/ "$PROJECT"
+ERRORS=$?
+echo "ERRORS = $ERRORS"
+if [ $ERRORS -ne 0 ]; then
+	echo "<testsuite tests='1'><testcase classname='markdownlint' name='DocChanges'><failure>$ERRORS markdownlint errors</failure></testcase></testsuite>" > markdownlint.junit.xml
+	exit $ERRORS
+fi
+
+# until markdownlint can output a junit.xml
+echo '<testsuite tests="1"><testcase classname="markdownlint" name="DocChanges"></testcase></testsuite>' > markdownlint.junit.xml
 
 echo "running a Hugo build"
 hugo --config=config.toml --log=true --stepAnalysis=true
+echo '<testsuite tests="1"><testcase classname="hugo" name="Build"></testcase></testsuite>' > hugo.junit.xml
+
